d70054a21a0cb525885f95a5589b51aa4cf3d5d1
leihao <leihao@gmail.com>
2021-08-28 18:02:09 +0800

    食材分类搜索框

---
+++ pages/index/categories.vue
@@ -0,0 +1,15 @@
+<template>
+  		<view class="searchBox">
+			<input type="text" placeholder="想吃什么搜这里，如麻婆豆腐" class="inputItem" />
+		</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+    @import './categories.scss';
+</style>
